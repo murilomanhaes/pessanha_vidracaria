54f6dd469bdfc879bca9fe50a714bf456a04c9d9
murilo_manhaes <murilo_manhaes@hotmail.com>
2012-08-30 23:03:32 -0300

    Primeiro commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PessanhaVidracaria::Application.config.secret_token = '4ff3aa690fe36194c3716d681dd3cf8fa1526e56142a064b89f5c223d7eb4bf5cbaae66ebdf35ad7c95da5b5f28728d601ef35efa0f0d0ceca884806b3e1ce82'
