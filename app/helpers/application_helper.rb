d8c1cb4dc528e67cfa39db936e2003a688d86d4c
murilo_manhaes <murilo_manhaes@hotmail.com>
2012-09-10 22:37:23 -0300

    Layout, usuario, home, etc

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,23 @@
 module ApplicationHelper
+
+  def opcoes_para_data
+    {size: 10, maxlength: 10, onkeypress: 'return mascaraData(this, event);'}
+  end
+
+  def opcoes_para_cpf
+    { size: 14, maxlength: 14, onkeypress: 'return mascaraCPF(this, event);'}
+  end
+
+  def opcoes_para_telefone
+    { size: 14, maxlength: 14, onkeypress: 'return mascaraTelefone(this, event);'}
+  end
+
+  def opcoes_para_cep
+    { size: 9, maxlength: 9, onkeypress: 'return mascaraCEP(this, event);'}
+  end
+
+  def opcoes_para_observacao
+    { cols: '33', rows: '5'}
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 PessanhaVidracaria::Application.routes.draw do
+  devise_for :usuarios
+
     resources :somos
     
     root to: "home#index"
